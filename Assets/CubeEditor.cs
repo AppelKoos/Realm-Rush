17e84f6c80cea69054491b6d69b4db78ee268191
Appelkoos <johanvdm2000@gmail.com>
2019-08-30 11:05:30 +0200

    [2] -Added textmesh labeling to cube prefab -Added textmesh to show cubes coordinate like a grid

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour
+public class CubeEditor : MonoBehaviour
 {
     [SerializeField][Range(1f,20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    private void Start()
+    {
+        
+    }
     void Update()
     {
         Vector3 snapPos;
+        textMesh = GetComponentInChildren<TextMesh>();
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         snapPos.y = Mathf.RoundToInt(transform.position.y / gridSize) * gridSize;
-
+        textMesh.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
         transform.position = new Vector3(snapPos.x, snapPos.y, snapPos.z);
     }
 }
